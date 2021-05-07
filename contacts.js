4f733b365e5205f80a4f084b87c6562e65ebb669
Dm3583 <pankovdm@gmail.com>
2021-05-07 05:09:45 -0700

    complite screenshots

--- contacts.js
+++ contacts.js
@@ -5,74 +5,75 @@ const shortid = require('shortid');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
-function chosenContact(contacts,contactId){
-  const contact = contacts.find(contact=>contact.id.toString()===contactId);
-  return contact;
+function chosenContact(contacts, contactId) {
+    const contact = contacts.find(contact => contact.id.toString() === contactId);
+    return contact;
 };
 
 function listContacts() {
-    return fs.readFile(contactsPath, 'utf8', (error, data)=>{
-      if(error) throw error;
-      return data;
-    })
-    .then(JSON.parse)
-    .catch(error=>console.error(error.message));;
-  }
-  
+    return fs.readFile(contactsPath, 'utf8', (error, data) => {
+            if (error) throw error;
+            return data;
+        })
+        .then(JSON.parse)
+        .catch(error => console.error(error.message));
+};
+
 function getContactById(contactId) {
     return listContacts()
-    .then(contacts=>{
-      const contactById = chosenContact(contacts,contactId)
-      return contactById?
-                contactById:
-              `There is no contact with id ${contactId}`.red})
-    .catch(error=>console.error(error.message));
-  }
-  
+        .then(contacts => {
+            const contactById = chosenContact(contacts, contactId);
+            return contactById ?
+                contactById :
+                `There is no contact with id ${contactId}`.red
+        })
+        .catch(error => console.error(error.message));
+};
+
 function removeContact(contactId) {
     return listContacts()
-    .then(contacts=>{
-      const contactToRemove = chosenContact(contacts, contactId);
-      console.log(contactToRemove);
-      if(!contactToRemove){
-        console.log(`There is no contact with id ${contactId}`.red);
-        return;
-      }
-      const filteredContacts = contacts.filter(contact=>contact.id.toString()!==contactId);
-      console.log('You just removed:'.yellow);
-      console.table(contactToRemove);
-      return filteredContacts;
-      })
-      .then(contacts=>{
-        if(!contacts){
-          return;
-        }
-        return fs.writeFile(contactsPath, JSON.stringify(contacts))})
-      .then(listContacts)
-    .catch(error=>console.error("Error message: ",error.message));
-  }
-  
+        .then(contacts => {
+            const contactToRemove = chosenContact(contacts, contactId);
+            if (!contactToRemove) {
+                console.log(`There is no contact with id ${contactId}`.red);
+                return;
+            }
+            const filteredContacts = contacts.filter(contact => contact.id.toString() !== contactId);
+            console.log('You just removed:'.yellow);
+            console.table(contactToRemove);
+            return filteredContacts;
+        })
+        .then(contacts => {
+            if (!contacts) {
+                return;
+            }
+            return fs.writeFile(contactsPath, JSON.stringify(contacts))
+        })
+        .then(listContacts)
+        .catch(error => console.error("Error message: ", error.message));
+};
+
 function addContact(name, email, phone) {
     return listContacts()
-      .then(contacts=>{
-        const contact = {
-          id: shortid.generate(),
-          name,
-          email,
-          phone
-        }
-        console.log('You add contact:'.green);
-        console.table(contact);
-        return [...contacts, contact];
-      })
-      .then(contacts=>fs.writeFile(contactsPath, JSON.stringify(contacts)))
-      .then(listContacts)
-    .catch(error=>console.error(error.message));
-  }
+        .then(contacts => {
+            const contact = {
+                id: shortid.generate(),
+                name,
+                email,
+                phone
+            }
+            console.log('You add contact:'.green);
+            console.table(contact);
+            return [...contacts, contact];
+        })
+        .then(contacts => fs.writeFile(contactsPath, JSON.stringify(contacts)))
+        .then(listContacts)
+        .catch(error => console.error(error.message));
+};
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
 };
--- index.js
+++ index.js
@@ -2,48 +2,41 @@ const { Command } = require('commander');
 const program = new Command();
 const contacts = require('./contacts');
 program
-  .option('-a, --action <type>', 'choose action')
-  .option('-i, --id <type>', 'user id')
-  .option('-n, --name <type>', 'user name')
-  .option('-e, --email <type>', 'user email')
-  .option('-p, --phone <type>', 'user phone');
+    .option('-a, --action <type>', 'choose action')
+    .option('-i, --id <type>', 'user id')
+    .option('-n, --name <type>', 'user name')
+    .option('-e, --email <type>', 'user email')
+    .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
 const argv = program.opts();
 
-// TODO: рефакторить
 function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case 'list':
-      // ...
-      contacts.listContacts()
-        .then(console.table);
-      break;
+    switch (action) {
+        case 'list':
+            contacts.listContacts()
+                .then(console.table);
+            break;
 
-    case 'get':
-      // ... id
-      contacts.getContactById(id)
-        .then(console.table)
-      break;
+        case 'get':
+            contacts.getContactById(id)
+                .then(console.table);
+            break;
 
-    case 'add':
-      // ... name email phone
-      contacts.addContact(name, email, phone)
-      .then(console.table);
-      ;
-      break;
+        case 'add':
+            contacts.addContact(name, email, phone)
+                .then(console.table);
+            break;
 
-    case 'remove':
-      // ... id
-      
-      contacts.removeContact(id)
-      .then(console.table);
-      break;
+        case 'remove':
+            contacts.removeContact(id)
+                .then(console.table);
+            break;
 
-    default:
-      console.warn('\x1B[31m Unknown action type!');
-  }
+        default:
+            console.warn('\x1B[31m Unknown action type!');
+    }
 }
 
 invokeAction(argv);
